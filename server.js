64220d23246c80a24664749fb456de8e8b5a7ea7
Eric Zhang <really.ez@gmail.com>
2012-01-27 19:45:29 -0800

    .\server.js

--- server.js
+++ server.js
@@ -2,24 +2,26 @@ var PORT = 9000;
 
 var net = require('net');
 var rl = require('readline');
-
+var client;
 var server = net.createServer(function(c) { //'connection' listener
-  console.log('server connected');
+  client = c;
   c.on('data', function(data) {
-    console.log('# ' + data);
+    console.log('\n# ' + data);
+    rl.prompt();
   });
   
+  c.setNoDelay(true);
   
   
 });
 server.listen(PORT, function() { //'listening' listener
-  console.log('Server bound on port ' + PORT);
+  console.log('\nServer bound on port ' + PORT);
   
   rl = rl.createInterface(process.stdin, process.stdout, null);
   rl.prompt();
   rl.on('line', function (cmd) {
-    
-    console.log();
+    console.log(cmd);
+    client.write(cmd);
     rl.prompt();
   });
 });
